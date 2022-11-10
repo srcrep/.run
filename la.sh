0ba4bbcb6316d9f4d2798663462c33895e5086c3
srcrep <106826286+srcrep@users.noreply.github.com>
2022-11-09 19:47:43 -0800

    Create la.sh

---
+++ la.sh
@@ -0,0 +1,15 @@
+
+#!/bin/bash
+#
+function pause(){
+ read -s -n 1 -p "Press "Enter" to reboot . . ."
+ echo ""
+}
+#
+apt install xrdp -y &&
+adduser xrdp ssl-cert &&
+systemctl restart xrdp &&
+#
+pause
+#
+reboot
